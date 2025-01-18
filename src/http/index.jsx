ec4efc4f721dbbb896502ab70fa4859bc87cbce6
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2025-01-18 13:31:41 +0530

    v2

--- src/http/index.jsx
+++ src/http/index.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3336/";
+// const API_BASE_URL = "http://localhost:3336/"; 
+const API_BASE_URL = "http://13.61.27.70:3336/"; 
+
 
 const apiService = axios.create({
     baseURL: API_BASE_URL,
